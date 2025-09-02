d74bf6e93fe708914d580628ac7672e2066794f6
Suin Kim <contact@suinkim.me>
2025-09-02 23:59:00 +0900

    feat: 펜, 선택 기능 구현

--- src/app/(main-task)/coaching-resume/_components/FabricCanvas.tsx
+++ src/app/(main-task)/coaching-resume/_components/FabricCanvas.tsx
@@ -1,10 +1,16 @@
 'use client';
 
+import { useEffect } from 'react';
+import * as fabric from 'fabric';
+
 // global hooks
 import { useWindowSize } from '@/hooks';
 
 // local hooks
 import { useFabricCanvas, useFabricDotGrid } from '../_hooks';
+
+// local stores
+import { useCanvasStore } from '../_stores';
 
 export function FabricCanvas() {
     const { width, height } = useWindowSize();
@@ -12,12 +18,27 @@ export function FabricCanvas() {
         width,
         height,
     });
+
+    // 배경 dot grid 그리는 훅
     useFabricDotGrid(canvas, {
         gap: 22,
         dot: 2,
         color: 'rgba(100,116,139,0.5)',
         opacity: 1,
     });
+
+    const setCanvasInstance = useCanvasStore((store) => store.setCanvasInstance);
+    useEffect(() => {
+        if (!canvas) {
+            return;
+        }
+
+        setCanvasInstance(canvas);
+
+        return () => {
+            setCanvasInstance(null);
+        };
+    }, [canvas, setCanvasInstance]);
 
     return <canvas ref={canvasRef} />;
 }
--- src/app/(main-task)/coaching-resume/_components/FabricToolbar.tsx
+++ src/app/(main-task)/coaching-resume/_components/FabricToolbar.tsx
@@ -1,16 +1,37 @@
+'use client';
+
 import { Flex, Button } from 'antd';
 
+// local stores
+import { useCanvasStore } from '../_stores';
+
 export function FabricToolbar() {
+    const isDrawingMode = useCanvasStore((s) => s.isDrawingMode);
+    const setDrawingMode = useCanvasStore((s) => s.setDrawingMode);
+    const hasCanvas = useCanvasStore((s) => !!s.canvasInstance);
+
     // TODO: 툴바 버튼 컴포넌트 분리 및 스타일링 필요
     return (
         <Flex
-            className='absolute bottom-[10px] left-[50%] transform-x-[-50%]'
+            className='absolute bottom-[10px] left-1/2 -translate-x-1/2 transform z-[10]'
             justify='center'
             align='center'
             gap={20}
         >
-            <Button>Pen</Button>
-            <Button>Eraser</Button>
+            <Button
+                disabled={!hasCanvas}
+                type={!isDrawingMode ? 'primary' : 'default'}
+                onClick={() => setDrawingMode(false)}
+            >
+                선택
+            </Button>
+            <Button
+                disabled={!hasCanvas}
+                type={isDrawingMode ? 'primary' : 'default'}
+                onClick={() => setDrawingMode(true)}
+            >
+                펜
+            </Button>
         </Flex>
     );
 }
