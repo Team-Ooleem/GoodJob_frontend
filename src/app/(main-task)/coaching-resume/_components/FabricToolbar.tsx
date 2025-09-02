9bb331cb62bf4c44dded5ce2cf464084d8f25ce2
Suin Kim <contact@suinkim.me>
2025-09-02 22:45:18 +0900

    docs: TODO 작성

--- src/app/(main-task)/coaching-resume/_components/FabricToolbar.tsx
+++ src/app/(main-task)/coaching-resume/_components/FabricToolbar.tsx
@@ -1,6 +1,7 @@
 import { Flex, Button } from 'antd';
 
 export function FabricToolbar() {
+    // TODO: 툴바 버튼 컴포넌트 분리 및 스타일링 필요
     return (
         <Flex
             className='absolute bottom-[10px] left-[50%] transform-x-[-50%]'
