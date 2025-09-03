092d0f63c8de37ee7e9b59aa8effeb56c51ef665
Suin Kim <contact@suinkim.me>
2025-09-04 02:50:43 +0900

    feat: 헤더 네비게이션 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,10 @@
-import { ZustandTest } from '@/components/zustand-test';
-import { ReactQueryTest } from '@/components/react-query-test';
+// global components
+import { Header } from '@/components';
 
 export default function Home() {
     return (
-        <div className='font-sans min-h-screen p-8 pb-20 gap-16 sm:p-20'>
-            <div className='max-w-6xl mx-auto space-y-8'>
-                <div className='text-center'>
-                    <h1 className='text-4xl font-bold text-gray-800 mb-4'>
-                        Good Job - AI Mock Interview Platform
-                    </h1>
-                    <p className='text-xl text-gray-600'>간소화된 테스트 페이지</p>
-                </div>
-
-                {/* Zustand 테스트 */}
-                <ZustandTest />
-
-                {/* React Query 테스트 */}
-                <ReactQueryTest />
-            </div>
-        </div>
+        <>
+            <Header />
+        </>
     );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+import { Flex } from 'antd';
+
+// global components
+import { HeaderNavigation } from './HeaderNavigation';
+
+export function Header() {
+    return (
+        <div className='w-full h-auto border-b-1 border-gray-200'>
+            <div className='mx-auto w-[1400px] h-[60px] flex justify-between items-center'>
+                <Flex justify='center' align='center' gap={70}>
+                    <Image
+                        src='/assets/good-job-logo.webp'
+                        alt='올인원 채용 플랫폼 굿잡'
+                        width={40}
+                        height={40}
+                    />
+                    <HeaderNavigation />
+                </Flex>
+            </div>
+        </div>
+    );
+}
