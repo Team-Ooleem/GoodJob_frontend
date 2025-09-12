a27d2bf1a2040f5ac679e71b554651b42cb70602
kko3093 <kko3093@gmail.com>
2025-09-12 21:30:53 +0900

    [feat] 멘토링 랜딩 페이지 디자인 및 퍼블리싱

---
+++ src/app/(platform)/mentoring/landing/page.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from 'next';
+import MentoringLandingContent from './components/MentoringLandingContent';
+
+export const metadata: Metadata = {
+    title: '멘토링 - GoodJob',
+    description: '가장 쉽게 지식을 공유하는 방법, 멘토링으로 시작하세요.',
+};
+
+export default function MentoringPage() {
+    return <MentoringLandingContent />;
+}
