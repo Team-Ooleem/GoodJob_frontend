82968da813421c06e7fd938a6992901024707bc7
omokgosu <anfakt0606@naver.com>
2025-09-15 03:42:58 +0900

    [ feat ] AI 인터뷰 이력서 새탭열림

--- src/components/HeaderNavigation.tsx
+++ src/components/HeaderNavigation.tsx
@@ -7,8 +7,12 @@ export function HeaderNavigation() {
     return (
         <div className='flex justify-center items-center gap-10'>
             <HeaderNavigationAnchor href='/'>채용</HeaderNavigationAnchor>
-            <HeaderNavigationAnchor href='/ai-interview'>AI 인터뷰</HeaderNavigationAnchor>
-            <HeaderNavigationAnchor href='/coaching-resume'>이력서</HeaderNavigationAnchor>
+            <HeaderNavigationAnchor href='/ai-interview' target='_blank'>
+                AI 인터뷰
+            </HeaderNavigationAnchor>
+            <HeaderNavigationAnchor href='/coaching-resume' target='_blank'>
+                이력서
+            </HeaderNavigationAnchor>
             <HeaderNavigationAnchor href='/social'>커뮤니티</HeaderNavigationAnchor>
         </div>
     );
